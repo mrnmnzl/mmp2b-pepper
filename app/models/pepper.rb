65da882de4991f0040df16056f2d20778849a7fe
Selina Nicole Brunner <sbrunner.mmt-b2016@fh-salzburg.ac.at>
2018-04-24 21:43:50 +0200

    add types

--- app/models/pepper.rb
+++ app/models/pepper.rb
@@ -1,3 +1,21 @@
 class Pepper < ApplicationRecord
   belongs_to :user
 end
+
+class Progress < Pepper
+end
+
+class Trend < Pepper
+end
+
+class ToDo < Pepper
+end
+
+class Mean < Pepper
+end
+
+class Negative < Pepper
+end
+
+class Piece < Pepper
+end
