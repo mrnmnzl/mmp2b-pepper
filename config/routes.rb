7da8358b6b0ec06a6e98aac9709367d9886748b6
Selina Nicole Brunner <sbrunner.mmt-b2016@fh-salzburg.ac.at>
2018-04-24 22:18:02 +0200

    add home peppers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :peppers
-  get 'main/index'
+  resources :main
   devise_for :users
   resources :users
-  resources :peppers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'main#index'
